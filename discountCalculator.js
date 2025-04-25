fbe41d9492d4fa8ca883e7f05e49948b1f8886ac
Marco Guadiana <marcoguadiana01@gmail.com>
2025-04-24 20:13:44 -0500

    4/24/25 8:13PM

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,20 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
-    }
+        console.log(totalPrice, " total price so far");
+        console.log(quantity, " so far items purchased");
 
+    }
+// If they are buying more than 10 items apply discount of 10% off
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
     return totalPrice;
 }
-
+console.log(calculateDiscountedPrice(2, 5));
 
 
 module.exports = calculateDiscountedPrice;
